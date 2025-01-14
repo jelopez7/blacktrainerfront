c87cc8d88f92901cfd53b7be0f26ed34da039e65
jesus.lopez <jesus.lopez@lasamericas.com.co>
2025-01-14 11:52:43 -0500

    Mostrar todos los usuarios

--- api/user.js
+++ api/user.js
@@ -3,7 +3,7 @@ import { httpConToken } from "../helpers/http";
 export async function getUsersApi() {
   try {
     const { data } = await httpConToken.get(
-      `/content-manager/collection-types/plugin::users-permissions.user`
+      `/content-manager/collection-types/plugin::users-permissions.user?pageSize=100`
     );
     return data;
   } catch (error) {
