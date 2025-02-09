cd86f9fecb18321777db8baee3e3a0e2ce4a40db
jesus.lopez <jesus.lopez@lasamericas.com.co>
2025-02-09 16:30:51 -0500

    Protegiendo la ruta

--- components/Courses/CoursesForm/CoursesForm.js
+++ components/Courses/CoursesForm/CoursesForm.js
@@ -5,8 +5,19 @@ import * as Yup from "yup";
 import "animate.css";
 import { useDispatch, useSelector } from "react-redux";
 import { addCourse } from "@/actions/course";
+import { useRouter } from "next/router";
 
 export default function CoursesForm({ setRenderComponent }) {
+  const { user } = useSelector((state) => state.auth);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user && typeof window !== "undefined") {
+      router.push("/auth"); // Redirige a la página de inicio de sesión si el usuario no está autenticado
+    }
+  }, [user, router]);
+
   const dispatch = useDispatch();
   const { status } = useSelector((state) => state.course);
 
